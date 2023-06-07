c038378e4e3b43070fab28b447d1b77e755e8ee6
Keaton Law <keaton.c.law@gmail.com>
2023-06-07 18:46:24 -0500

    serverbasic

---
+++ main.py
@@ -0,0 +1,87 @@
+import json
+import openai
+import requests
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
+
+class HandleRequests(BaseHTTPRequestHandler):
+
+    def parse_url(self, path):
+        """Parse the url into the resource and id"""
+        parsed_url = urlparse(path)
+        path_params = parsed_url.path.split('/')  # ['', 'animals', 1]
+        resource = path_params[1]
+
+        if parsed_url.query:
+            query = parse_qs(parsed_url.query)
+            return (resource, query)
+
+        pk = None
+        try:
+            pk = int(path_params[2])
+        except (IndexError, ValueError):
+            pass
+        return (resource, pk)
+
+    # Here's a class function
+    def _set_headers(self, status):
+        # Notice this Docstring also includes information about the arguments passed to the function
+        """Sets the status code, Content-Type and Access-Control-Allow-Origin
+        headers on the response
+
+        Args:
+            status (number): the status code to return to the front end
+        """
+        self.send_response(status)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+    # Another method! This supports requests with the OPTIONS verb.
+    def do_OPTIONS(self):
+        """Sets the options headers
+        """
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods',
+                        'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Headers',
+                        'X-Requested-With, Content-Type, Accept')
+        self.end_headers()
+
+    def do_POST(self):
+        self._set_headers(201)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new data
+        if resource == "chat":
+            #request to openAI
+            openai.api_key = os.environ.get("OPENAI_APIKEY")
+            messagearr=post_body["conversation"]
+            messagearr.insert(0, { "role": "system", "content": "You are a snarky chatbot. You will only respond with witty insults" })
+            completion = openai.ChatCompletion.create(
+              model="gpt-3.5-turbo",
+              temperature=1.5,
+              messages=messagearr)
+            print(completion.choices[0].message.content)
+            self.wfile.write(json.dumps(completion.choices[0].message.content).encode())
+
+def main():
+    """Starts the server on port 8088 using the HandleRequests class
+    """
+    host = ''
+    port = 8088
+    HTTPServer((host, port), HandleRequests).serve_forever()
+
+
+if __name__ == "__main__":
+    main()
